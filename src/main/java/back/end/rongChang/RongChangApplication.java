0da391dd1e22edec3b01304318c521b080e3784f
Mohamed BANHAKEIA <mohamed.banhakeia@hotmail.com>
2021-08-25 11:41:07 +0200

    Audios and Images has been added to the project

---
+++ src/main/java/back/end/rongChang/RongChangApplication.java
@@ -0,0 +1,13 @@
+package back.end.rongChang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RongChangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RongChangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/back/end/rongChang/RongChangApplicationTests.java
@@ -0,0 +1,13 @@
+package back.end.rongChang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RongChangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
